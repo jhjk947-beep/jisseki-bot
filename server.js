ecc92dcaf6ea8fcc571a31e4f2562045999bf506
jhjk947-beep <jhjk947@gmail.com>
2026-05-27 18:53:10 +0900

    Update server.js

--- server.js
+++ server.js
@@ -12,3 +12,22 @@ app.post("/login", (req, res) => {
 });
 
 app.listen(3000);
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("サーバー動いてる！");
+});
+
+app.post("/login", (req, res) => {
+  console.log(req.body);
+
+  res.json({
+    message: "OK"
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
